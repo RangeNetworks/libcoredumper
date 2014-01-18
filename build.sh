1329fdd2c9e9455b77534f61f8bdc0fcca060cc8
Michael Iedema <michael.iedema@rangenetworks.com>
2014-01-18 08:40:51 +0000

    - libcoredumper stub added which builds from scratch both lib and dev packages - this meta package follows the same pattern as our asterisk, libzmq, libsqliteodbc packages - they're being made uniform so we can transition them all at the same time for 4.1 so we have two package build processes: range & 3rd party
    git-svn-id: http://wush.net/svn/range/software/commercial/libcoredumper/trunk@7323 19bc5d8c-e614-43d4-8b26-e1612bc8e597

---
+++ build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+VERSION=1.2.1
+
+sayAndDo () {
+	echo $@
+	eval $@
+	if [ $? -ne 0 ]
+	then
+		echo "ERROR: command failed!"
+		exit 1
+	fi
+}
+
+installIfMissing () {
+	dpkg -s $@ > /dev/null
+	if [ $? -ne 0 ]; then
+		echo " - oops, missing $@, installing"
+		sudo apt-get install $@
+	else
+		echo " - $@ ok"
+	fi
+	echo
+}
+
+if [ ! -f coredumper-$VERSION.tar.gz ]
+then
+	sayAndDo wget http://google-coredumper.googlecode.com/files/coredumper-$VERSION.tar.gz
+fi
+
+if [ -d coredumper-$VERSION ]
+then
+	sayAndDo rm -rf coredumper-$VERSION
+fi
+
+sayAndDo tar zxf coredumper-$VERSION.tar.gz
+sayAndDo cd coredumper-$VERSION
+sayAndDo mv packages/deb debian
+sayAndDo patch -p0 < ../fix_from_scratch_build.patch
+sayAndDo dpkg-buildpackage
